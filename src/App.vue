a9ffb942856bb4b2bcf49b692aa0cc993d8327b3
tcly861204 <356671808@qq.com>
2022-11-30 23:11:24 +0800

    ✨ feat: 完善前端

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    HelloWorld
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
